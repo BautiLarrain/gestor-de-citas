fe7e24b6432378e5758647b7236b10b0fc218efa
GUIDO SCHULMAN <46961979@ort>
2023-06-15 09:07:09 -0300

    funciona

--- src/componentes/Cita.js
+++ src/componentes/Cita.js
@@ -1,24 +1,22 @@
 
 import React from 'react'
 
-export default function Cita({cita,lista,setLista}) {
+export default function Cita({cita,lista,setLista,eliminar}) {
   const {nombre,propietario,fecha,hora,sintomas,id} = cita;
   
-  const Eliminar=(id)=>{
-    
-    const listaNueva=lista.filter(Cita=>Cita.id != id)
-    
-    //setLista([...lista,listaNueva]);
-    
+
+  const Elim = (id) => {
+    eliminar(lista,setLista,id)
+   
   }
+ 
   return (
-    <div class="cita">
+    <div className="cita">
     <p>Mascota: <span>{nombre}</span></p>
     <p>Dueño: <span>{propietario}</span></p>
     <p>Fecha: <span>{fecha}</span></p>
     <p>Hora: <span>{hora}</span></p>
     <p>Sintomas: <span>{sintomas}</span></p>
-    <button class="button elimnar u-full-width" onChange={Eliminar(id)}>Eliminar ×</button>
-  </div>
+<button className="button eliminar u-full-width" onClick={() => Elim(id)}>Eliminar</button>  </div>
   )
 }
--- src/componentes/ListadoCitas.js
+++ src/componentes/ListadoCitas.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Cita from "./Cita";
 
+const Eliminar = (lista, setLista, id) => {
+  const listaNueva = lista.filter((cita) => cita.id !== id);
+  setLista(listaNueva);
+};
 
-const ListadoCitas = ({setLista,lista }) => {
-    return (
-      <ul>
-        { lista.map((cita) => (
-          
-          <Cita 
+const ListadoCitas = ({ setLista, lista }) => {
+  console.log(lista);
+
+  if (!Array.isArray(lista)) {
+    return <div>Loading...</div>; // or any loading indicator or null if you want to render nothing
+  }
+
+  return (
+    <ul>
+      {lista.map((cita) => (
+        <Cita
+          key={cita.id}
+          idCita={cita.id}
           cita={cita}
           lista={lista}
-          setLista={setLista}  />
-   
-        ))}
-
-      </ul>
-      
-    );
-  };
+          setLista={setLista}
+          eliminar={Eliminar}
+        />
+      ))}
+    </ul>
+  );
+};
   
    
   export default ListadoCitas
